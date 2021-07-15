de5d8a610c1585bb9a7afc5db042d943e2783c39
mosky <smartmosky@gmail.com>
2021-07-15 18:45:02 -0400

    Added some graphics, not optomized

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 console.log('Huh!?!?')
-const homeContent = document.querySelector('#homeContent');
-const menuContent = document.querySelector('#menuContent');
+
 const content = document.querySelector('#content');
 content.style.display = 'flex';
 content.style.alignItems = 'center';
@@ -12,16 +11,32 @@ content.appendChild(topbar);
 topbar.style.backgroundColor = 'lightgreen';
 topbar.style.height = '50px';
 topbar.style.width = '100%';
+topbar.style.display = 'flex';
+topbar.style.justifyContent = 'space-evenly';
 
+const realContent = document.createElement('div');
+realContent.id = 'realContent'
+const leftContent = document.createElement('div');
+const rightContent = document.createElement('div');
 const midContent = document.createElement('div');
 midContent.id = 'midContent';
-content.appendChild(midContent);
+content.appendChild(realContent)
+realContent.appendChild(leftContent)
+realContent.appendChild(midContent);
+realContent.appendChild(rightContent)
+
+leftContent.classList.add('sideContent')
+rightContent.classList.add('sideContent')
+leftContent.textContent = 'TASTE'
+rightContent.textContent = 'CITY'
+
+
 midContent.style.display = 'flex';
 midContent.style.flexDirection = 'column';
 midContent.style.alignItems = 'center';
 midContent.style.backgroundColor ='black'
 midContent.style.color = 'white';
-midContent.style.width = '80%';
+
 
 
 
